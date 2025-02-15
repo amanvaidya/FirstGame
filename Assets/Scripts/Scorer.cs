784c308f83b13a517f1987fb79ef88ed3ada8b6e
amanvaidya <amanvaidya700@gmail.com>
2025-02-15 17:42:27 +0530

    Added tags

--- Assets/Scripts/Scorer.cs
+++ Assets/Scripts/Scorer.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
 
-public class Scro : MonoBehaviour
+public class Scorer : MonoBehaviour
 {
     int hits = 0;
    private void OnCollisionEnter(Collision other) {
+    Debug.Log("other.gameObject.tag: " + other.gameObject.tag);
      if(other.gameObject.tag != "Hit")
      {    
           Debug.Log("You've bumped into a thing this many times: " + hits++);
