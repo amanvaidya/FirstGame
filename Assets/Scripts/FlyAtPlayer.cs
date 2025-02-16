69d6692b7618d3e9a274b6be083796e0f6cc14e7
amanvaidya <amanvaidya700@gmail.com>
2025-02-16 17:43:49 +0530

    Added prefabs

--- Assets/Scripts/FlyAtPlayer.cs
+++ Assets/Scripts/FlyAtPlayer.cs
@@ -4,8 +4,12 @@ public class FlyAtPlayer : MonoBehaviour
 {
     [SerializeField] Transform player;
     Vector3 playerPosition;
-    [SerializeField] float speed = 0.01f;
+    [SerializeField] float speed = 10f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    void Awake() {
+        gameObject.SetActive(false);
+    }
     void Start()
     {
         playerPosition = player.transform.position;
@@ -14,12 +18,14 @@ public class FlyAtPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("Projectile activated inside Update()");
         MoveToPlayer();
         DestroyWhenReached();
     }
 
     void MoveToPlayer(){
         //playerPosition = player.transform.position;//this is useful if we want to follow the player
+        Debug.Log("Projectile set to active inside MoveToPlayer()");
         transform.position = Vector3.MoveTowards(transform.position, playerPosition, Time.deltaTime * speed);
     }
     void DestroyWhenReached(){
--- Assets/Scripts/TriggerProjectile.cs
+++ Assets/Scripts/TriggerProjectile.cs
@@ -5,12 +5,18 @@ public class TriggerProjectile : MonoBehaviour
     [SerializeField] GameObject[] projectiles;
 
     private void OnTriggerEnter(Collider other) {
+        Debug.Log("Tag"+ other.gameObject.tag);
         if(other.gameObject.tag == "Player")
-        {
+        {   
+            Debug.Log("Projectiles"+ projectiles.Length);
             foreach (GameObject projectile in projectiles)
             {
+                Debug.Log("Projectile activated"+ projectile);
                 projectile.SetActive(true);
+                Debug.Log("Projectile set to active");
             }
+            Debug.Log("TriggerProjectile destroyed");
+            // Destroy(gameObject);
         }
     }
 }
